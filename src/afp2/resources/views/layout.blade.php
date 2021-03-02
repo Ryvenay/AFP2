b51586ed0e1a7163544ccdfc18fb77589948ee28
Tóbi Csanádi <csanadi.tobias@gmail.com>
2021-03-02 16:12:59 +0100

    Added layout file
    The layout file has been added. Footer moved to this file

---
+++ src/afp2/resources/views/layout.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+        <!-- Styles -->
+         <link href="\css\style.css" rel="stylesheet" rel="text/css">
+    </head>
+    <body class="antialiased">
+
+        @yield('content')
+
+
+        <div class="flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:pt-0">
+            <div class="mt-2 text-gray-600 dark:text-gray-400 text-sm">
+                <center>Copyright &copy; 2021 <b> IMPACT ZONE </b> TEAM </center>
+            </div>
+        </div>
+    </body>
+</html>
