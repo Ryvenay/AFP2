5b4495f9e46393ea95cabcbdc446795049cb35d2
Tóbi Csanádi <csanadi.tobias@gmail.com>
2021-03-04 17:56:58 +0100

    Added create and edit routes for product
    The routing for the create and edit view has been added to the web routes file.

--- src/afp2/routes/web.php
+++ src/afp2/routes/web.php
@@ -23,4 +23,6 @@ Route::get('/', function () {
 });
 
 Route::get('/products', [ProductController::class, 'index']);
+Route::get('/products/create', [ProductController::class, 'create']);
+Route::get('/products/edit', [ProductController::class, 'edit']);
 Route::get('/products/{product}', [ProductController::class, 'show']);
