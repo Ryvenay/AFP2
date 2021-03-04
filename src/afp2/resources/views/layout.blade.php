c2ef682bf3bfb9871ade7f9b051411e9e3e894f0
agnespapp0729 <agnespapp99@gmail.com>
2021-03-04 12:27:02 +0100

    Added URI to the navbar menu items

--- src/afp2/resources/views/layout.blade.php
+++ src/afp2/resources/views/layout.blade.php
@@ -19,25 +19,34 @@
     <div class="collapse navbar-collapse" id="navbarNav">
         <ul class="navbar-nav">
             <li class="nav-item active">
-                <a class="nav-link" id="nav-home" href="">Home</a>
+                <a class="nav-link" id="nav-home" href="/">Home</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="aboutme">Products1</a>
+                <a class="nav-link" href="/products">Products1</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="aboutme">Products2</a>
+                <a class="nav-link" href="/products">Products2</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="aboutme">Products3</a>
+                <a class="nav-link" href="/products">Products3</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="aboutme">Login</a>
+                <a class="nav-link" href="/login">Login</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="aboutme">Logout</a>
+                <a class="nav-link" href="/logout">Logout</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="aboutme">Register</a>
+                <a class="nav-link" href="/register">Register</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="/addProduct">Add a product</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="/orders">Order Listing</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="/cart">Cart</a>
             </li>
             <li class="nav-item">
                 <a class="nav-link" href="cv"></a>
