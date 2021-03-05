163ec1e5ffc5efb0d55dd183d753e92954bb0a36
Tóbi Csanádi <csanadi.tobias@gmail.com>
2021-03-05 15:41:37 +0100

    Extended routings for Products
    The web routes for the products has been extended with all REST methods

--- src/afp2/routes/web.php
+++ src/afp2/routes/web.php
@@ -24,5 +24,9 @@ Route::get('/', function () {
 
 Route::get('/products', [ProductController::class, 'index']);
 Route::get('/products/create', [ProductController::class, 'create']);
-Route::get('/products/edit', [ProductController::class, 'edit']);
+Route::post('/products', [ProductController::class, 'store']);
+Route::get('/products/{product}/edit', [ProductController::class, 'edit']);
 Route::get('/products/{product}', [ProductController::class, 'show']);
+Route::put('/products/{product}', [ProductController::class, 'update'])
+Route::delete('/products/{product}', [ProductController::class, 'delete']);
+
