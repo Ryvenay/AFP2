71993ab579fa5ab41c0054e0b41eda6d1d52dd47
Tóbi Csanádi <csanadi.tobias@gmail.com>
2021-03-07 11:59:41 +0100

    Fixed error in ProductController

--- src/afp2/app/Http/Controllers/ProductController.php
+++ src/afp2/app/Http/Controllers/ProductController.php
@@ -37,7 +37,7 @@ class ProductController extends Controller
     }
 
     public function delete($id) {
-        $success = Product::where('id' $id);
+        $success = Product::where('id', $id);
         return $success;
     }
 
