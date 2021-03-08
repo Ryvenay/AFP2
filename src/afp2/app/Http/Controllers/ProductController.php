d413a32d9e6aa51e636131318ac640751521a441
Tóbi Csanádi <csanadi.tobias@gmail.com>
2021-03-08 10:57:58 +0100

    Fixed error in Store method
    The object type has been exchanged to Product type

--- src/afp2/app/Http/Controllers/ProductController.php
+++ src/afp2/app/Http/Controllers/ProductController.php
@@ -29,7 +29,7 @@ class ProductController extends Controller
     }
 
     public function store() {
-        Article::create(request()->validate([
+        Product::create(request()->validate([
             'name' => 'required',
             'brand' => 'required',
             'category' => 'required',
