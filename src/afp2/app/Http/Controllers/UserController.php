b50a3f1ae8669b7c568021340ae9b4995e39fa35
Tóbi Csanádi <csanadi.tobias@gmail.com>
2021-03-09 19:49:41 +0100

    Added logout function to UserContoller
    The method logs users out when routed here.

--- src/afp2/app/Http/Controllers/UserController.php
+++ src/afp2/app/Http/Controllers/UserController.php
@@ -30,3 +30,16 @@ class UserController extends Controller
     public function login() {
         return view('users.login');
     }
+
+    public function logout(Request $request) {  
+        Auth::logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        return redirect('/');
+    }
+
+
+}
