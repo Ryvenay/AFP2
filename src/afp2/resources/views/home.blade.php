e00434367a6ea5169f39122227a2b0421e832728
Tóbi Csanádi <csanadi.tobias@gmail.com>
2021-03-09 19:48:27 +0100

    Modified home page greeting text
    It now shoes the first name of the user if logged in. Otherwise it writes 'visitor'

--- src/afp2/resources/views/home.blade.php
+++ src/afp2/resources/views/home.blade.php
@@ -2,5 +2,12 @@
 
 
 @section('content')
-    <h1>Hello</h1>
+    <h1>Hello,  
+        @if (Auth::check())
+            {{ Auth::user()->last_name }}
+        @else
+            Visitor!
+        @endif
+    
+    </h1>
 @endsection
