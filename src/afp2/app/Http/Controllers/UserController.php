d142a01d79c8e2a61e00203b90355be89416ae6c
Tóbi Csanádi <csanadi.tobias@gmail.com>
2021-03-09 16:59:54 +0100

    Added authenticate method to UserController

--- src/afp2/app/Http/Controllers/UserController.php
+++ src/afp2/app/Http/Controllers/UserController.php
@@ -4,8 +4,21 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 
 class UserController extends Controller
 {
-   
+   public function authenticate(Request $request) {
+        $credentials = $request->only('email', 'password');
+
+        if (Auth::attempt($credentials)) {
+            $request->session()->regenerate();
+
+            return redirect()->intended('/');
+        }
+
+        return back()->withErrors([
+            'email' => 'The provided credentials do not match our records.',
+        ]);
+   }
 }
