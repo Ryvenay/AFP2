6d4e4e326793b01554872229c3c6cb78f10a0e81
Fügedi Csaba <fugedic@gmail.com>
2021-03-09 11:43:22 +0100

    Created Cart model

--- src/afp2/app/Models/cart.php
+++ src/afp2/app/Models/cart.php
@@ -5,7 +5,17 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class cart extends Model
+class Cart extends Model
 {
     use HasFactory;
+
+    public function UserId()
+    {
+        return $this->hasOne(User::class,'id','user_id');
+    }
+
+    public function ProductId()
+    {
+        return $this->hasOne(Product::class,'id','product_id');
+    }
 }
