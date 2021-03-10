b2e35653b366df4ebe0004de24bc662faa7cf087
agnespapp0729 <agnespapp99@gmail.com>
2021-03-10 17:17:18 +0100

    Added a new item and a new image

--- src/afp2/resources/views/products/list.blade.php
+++ src/afp2/resources/views/products/list.blade.php
@@ -54,9 +54,9 @@
 </div>
 <div class="col-md-4">
 <div class="card" style="width: 18rem;">
-  <img src="..." class="card-img-top" alt="...">
+  <img src="img/product/network_card.jpg" class="card-img-top"  width="190" height="200" alt="...">
   <div class="card-body">
-    <h5 class="card-title">Card title</h5>
+    <h5 class="card-title">Intel Network Adapter Card</h5>
     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
     <a href="#" class="btn btn-primary">Detailed description</a>
   </div>
