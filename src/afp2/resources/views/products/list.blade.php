a975ed511a84d8595c4b85bc06648866acb849ff
agnespapp0729 <agnespapp99@gmail.com>
2021-03-10 16:18:01 +0100

    Added some description to a product

--- src/afp2/resources/views/products/list.blade.php
+++ src/afp2/resources/views/products/list.blade.php
@@ -36,7 +36,7 @@
   <img src="img/product/hp_laptop.jpg" width="200" height="200" class="card-img-top" alt="...">
   <div class="card-body">
     <h5 class="card-title">HP EliteBook 840 G1 laptop</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <p class="card-text">Windows 10 Home 64. AMD Ryzen™ 3 processor. Free shipping. Extra 5% off select monitors with purchase of a PC.</p>
     <a href="#" class="btn btn-primary">Detailed description</a>
   </div>
 </div>
