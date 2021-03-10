043d17fff45ba105722ae5f2e45eacce77cc1a25
agnespapp0729 <agnespapp99@gmail.com>
2021-03-10 14:29:55 +0100

    Added a new picture and a title

--- src/afp2/resources/views/products/list.blade.php
+++ src/afp2/resources/views/products/list.blade.php
@@ -14,9 +14,9 @@
 </div>
 <div class="col-md-4">
 <div class="card" style="width: 18rem;">
-  <img src="..." class="card-img-top" alt="...">
+  <img src="img/product/dell_laptop.jpg" class="card-img-top" alt="...">
   <div class="card-body">
-    <h5 class="card-title">Card title</h5>
+    <h5 class="card-title">Dell Latitude E5450 laptop</h5>
     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
     <a href="#" class="btn btn-primary">Go somewhere</a>
   </div>
