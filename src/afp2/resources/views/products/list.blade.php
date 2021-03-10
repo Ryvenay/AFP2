fba3327e94be05cc29231cecd58b66f8263178f1
agnespapp0729 <agnespapp99@gmail.com>
2021-03-10 15:17:45 +0100

    Resized the pictures and added some description

--- src/afp2/resources/views/products/list.blade.php
+++ src/afp2/resources/views/products/list.blade.php
@@ -4,7 +4,7 @@
 <div class="row">
 <div class="col-md-4">
 <div class="card" style="width: 18rem;">
-  <img src="img/product/remote_c.jpg" class="card-img-top" alt="...">
+  <img src="img/product/remote_c.jpg" class="card-img-top" width="200" height="250"  alt="...">
   <div class="card-body">
     <h5 class="card-title">Philips remote control</h5>
     <p class="card-text">Brand new remote control for Philips TV-s. No programming required. Silicone rubber button. 30 Days Money Back Guarantee.1 Year Full Warranty</p>
@@ -14,10 +14,10 @@
 </div>
 <div class="col-md-4">
 <div class="card" style="width: 18rem;">
-  <img src="img/product/dell_laptop.jpg" class="card-img-top" alt="...">
+  <img src="img/product/dell_laptop.jpg" class="card-img-top" width="200" height="250"  alt="...">
   <div class="card-body">
     <h5 class="card-title">Dell Latitude E5450 laptop</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <p class="card-text"> It was designed to keep end-users productive. This laptop offers more options of ports, storage and connectivity than any other Latitude</p>
     <a href="#" class="btn btn-primary">Detailed description</a>
   </div>
 </div>
