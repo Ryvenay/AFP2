eb36b046332402f6db4d334256177f42f8442610
agnespapp0729 <agnespapp99@gmail.com>
2021-03-10 13:58:17 +0100

    Added a picture and a new folder for pictures

--- src/afp2/resources/views/products/list.blade.php
+++ src/afp2/resources/views/products/list.blade.php
@@ -4,9 +4,9 @@
 <div class="row">
 <div class="col-md-4">
 <div class="card" style="width: 18rem;">
-  <img src="..." class="card-img-top" alt="...">
+  <img src="images/remote_c.jpg" class="card-img-top" alt="...">
   <div class="card-body">
-    <h5 class="card-title">Card title</h5>
+    <h5 class="card-title">Philips remote control</h5>
     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
     <a href="#" class="btn btn-primary">Go somewhere</a>
   </div>
