12b11108731c571e71a800a5cf590f95876585af
agnespapp0729 <agnespapp99@gmail.com>
2021-03-10 14:22:40 +0100

    Added some description to one product

--- src/afp2/resources/views/products/list.blade.php
+++ src/afp2/resources/views/products/list.blade.php
@@ -7,7 +7,7 @@
   <img src="img/product/remote_c.jpg" class="card-img-top" alt="...">
   <div class="card-body">
     <h5 class="card-title">Philips remote control</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <p class="card-text">Brand new remote control for Philips TV-s. No programming required. Silicone rubber button. 30 Days Money Back Guarantee.1 Year Full Warranty</p>
     <a href="#" class="btn btn-primary">Go somewhere</a>
   </div>
 </div>
