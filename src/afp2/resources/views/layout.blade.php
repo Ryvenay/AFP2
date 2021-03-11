596b05e342ff8b4eb6ed364c029d8d9d487b6bf2
Tóbi Csanádi <csanadi.tobias@gmail.com>
2021-03-11 13:03:18 +0100

    Changed bootstrap JS version
    The JS version of the bootstrap has been updated

--- src/afp2/resources/views/layout.blade.php
+++ src/afp2/resources/views/layout.blade.php
@@ -13,55 +13,54 @@
             integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
 
         <!-- Js -->
-        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"
-            integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6"
-            crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js"
+             integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
     </head>
     
     <body class="antialiased">
     <nav class="navbar navbar-expand-lg">
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
-            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav">
-            <li class="nav-item active">
-                <a class="nav-link" id="nav-home" href="/">Home</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="/products">Products1</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="/products">Products2</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="/products">Products3</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="/login">Login</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="/logout">Logout</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="/register">Register</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="/addProduct">Add a product</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="/orders">Order Listing</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="/cart">Cart</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="cv"></a>
-            </li>
-        </ul>
-    </div>
-</nav>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item active">
+                    <a class="nav-link" id="nav-home" href="/">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/products">Products1</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/products">Products2</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/products">Products3</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/login">Login</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/logout">Logout</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/register">Register</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/addProduct">Add a product</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/orders">Order Listing</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/cart">Cart</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="cv"></a>
+                </li>
+            </ul>
+        </div>
+    </nav>
     @yield('content')
        
 
