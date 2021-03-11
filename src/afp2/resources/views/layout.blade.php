a0a4ab4bc12bc487207214ef69e74cb533646375
Tóbi Csanádi <csanadi.tobias@gmail.com>
2021-03-11 13:19:55 +0100

    Changed CSS priority

--- src/afp2/resources/views/layout.blade.php
+++ src/afp2/resources/views/layout.blade.php
@@ -8,9 +8,10 @@
         
         <!-- Styles -->
 
-        <link href="css/style.css" rel="stylesheet" type="text/css">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet"
             integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+        <link href="css/style.css" rel="stylesheet" type="text/css">
+        
 
         <!-- Js -->
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js"
