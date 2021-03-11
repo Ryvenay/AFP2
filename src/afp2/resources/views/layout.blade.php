fc1f98eb7ebe7848383f787db130b6c6db677593
Tóbi Csanádi <csanadi.tobias@gmail.com>
2021-03-11 13:25:17 +0100

    Moved navbar to separate file

--- src/afp2/resources/views/layout.blade.php
+++ src/afp2/resources/views/layout.blade.php
@@ -19,49 +19,7 @@
     </head>
     
     <body class="antialiased">
-    <nav class="navbar navbar-expand-lg">
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
-                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav">
-                <li class="nav-item active">
-                    <a class="nav-link" id="nav-home" href="/">Home</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="/products">Products1</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="/products">Products2</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="/products">Products3</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="/login">Login</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="/logout">Logout</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="/register">Register</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="/addProduct">Add a product</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="/orders">Order Listing</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="/cart">Cart</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="cv"></a>
-                </li>
-            </ul>
-        </div>
-    </nav>
+    @include('nav')
     @yield('content')
        
 
