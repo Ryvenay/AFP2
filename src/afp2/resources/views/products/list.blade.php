7131e63912c57602a24b6b0074734bf480c3579c
Tóbi Csanádi <csanadi.tobias@gmail.com>
2021-03-11 13:07:07 +0100

    Modified product listing view
    The listing now shows the brand too

--- src/afp2/resources/views/products/list.blade.php
+++ src/afp2/resources/views/products/list.blade.php
@@ -10,7 +10,7 @@
       <div class="card" style="width: 18rem;">
         <img src={{ $product->img }} class="card-img-top" width="200" height="250"  alt="...">
         <div class="card-body">
-          <h5 class="card-title">{{ $product->name }}</h5>
+          <h5 class="card-title">{{ $product->brand}} {{ $product->name }}</h5>
           <h6 class="card-subtitle mb-2 text-muted">{{$product->price}}</h6>
           <p class="card-text">{{$product->s_description}}</p>
 
