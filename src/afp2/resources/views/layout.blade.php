7b354f2dcfefbd666a17c5d5837c58b03f121ff1
Tóbi Csanádi <csanadi.tobias@gmail.com>
2021-03-12 11:07:47 +0100

    Edited navbar color scheme

--- src/afp2/resources/views/layout.blade.php
+++ src/afp2/resources/views/layout.blade.php
@@ -20,8 +20,8 @@
     @yield('content')
        
 
-        <div class="flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:pt-0">
-            <div class="mt-2 text-gray-600 dark:text-gray-400 text-sm">
+        <div class="flex items-top justify-center min-h-screen bg-dark dark:bg-gray-900 sm:pt-0">
+            <div class="mt-2 text-primary dark:text-gray-400 text-sm">
                 <center>Copyright &copy; 2021 <b> IMPACT ZONE </b> TEAM </center>
             </div>
         </div>
