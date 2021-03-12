56558c2dce229ab70bde5dfe846139ae397a289e
Tóbi Csanádi <csanadi.tobias@gmail.com>
2021-03-12 19:04:11 +0100

    Added default value for user level.

--- src/afp2/database/migrations/2021_03_01_203209_create_users_table.php
+++ src/afp2/database/migrations/2021_03_01_203209_create_users_table.php
@@ -23,7 +23,7 @@ class CreateUsersTable extends Migration
             $table->date('birthdate');
             $table->unsignedBigInteger('billing_address');
             $table->unsignedBigInteger('shipping_address');
-            $table->timestamp('regdate');
+            $table->tinyinteger('level')->default('0');
             $table->string('remember_token',100)->nullable();
             $table->timestamp('updated_at')->nullable();
             $table->timestamp('created_at')->nullable();
