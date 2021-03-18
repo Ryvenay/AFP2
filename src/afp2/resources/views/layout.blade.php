157f09c5077d5558f7a6ed1df7ff59e0a114c2b8
Fügedi Csaba <fugedic@gmail.com>
2021-03-18 14:24:00 +0100

    Minor changes on layout.blade

--- src/afp2/resources/views/layout.blade.php
+++ src/afp2/resources/views/layout.blade.php
@@ -13,17 +13,16 @@
         <!-- Js -->
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js"
              integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
+    
     </head>
     
-    <body class="antialiased">
+    <body>
     @include('nav')
     @yield('content')
        
 
-        <div class="flex items-top justify-center min-h-screen bg-dark dark:bg-gray-900 sm:pt-0">
-            <div class="mt-2 text-primary dark:text-gray-400 text-sm">
+        <div class="footer_div">
                 <center>Copyright &copy; 2021 <b> IMPACT ZONE </b> TEAM </center>
-            </div>
         </div>
     </body>
 </html>
