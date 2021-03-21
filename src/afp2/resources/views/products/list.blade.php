49f9266d0c47312803bd016e0bc6bbf6c3dd402f
Tóbi Csanádi <csanadi.tobias@gmail.com>
2021-03-21 13:49:20 +0100

    Added links for cards

--- src/afp2/resources/views/products/list.blade.php
+++ src/afp2/resources/views/products/list.blade.php
@@ -1,23 +1,23 @@
 @extends('layout')
 @section('content')
 
-<div class="container">
+<div class="container items">
   <div class="row">
 
 
   @foreach ($products as $product)
-    <div class="col-md-3">
-      <div class="card" style="width: 18rem;">
-      <div class="card-img text-center">
-        <img src={{ $product->img }} class="card-img-top" alt="...">
-      </div>
-        <div class="card-body">
-          <h5 class="card-title">{{ $product->brand}} {{ $product->name }}</h5>
-          <h6 class="card-subtitle mb-2 text-muted">{{$product->price}} Ft</h6>
-          <p class="card-text">{{$product->s_description}}</p>
-
+    <div class="col-md-auto">
+        <div class="card">
+          <div class="card-img text-center">
+            <img src={{ $product->img }} class="card-img-top" alt="...">
+          </div>
+          <div class="card-body">
+            <h5 class="card-title">{{ $product->brand}} {{ $product->name }}</h5>
+            <h6 class="card-subtitle mb-2 text-muted">{{$product->price}} Ft</h6>
+            <p class="card-text">{{$product->s_description}}</p>
+          </div>
+          <a href="/products/{{$product->id}}" role="button" class="stretched-link"></a>
         </div>
-      </div>
     </div>
   @endforeach
 
