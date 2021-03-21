fcdda0a9c7e2b8262f44448db39de061e3201491
Tóbi Csanádi <csanadi.tobias@gmail.com>
2021-03-21 16:37:12 +0100

    Extended product page

--- src/afp2/resources/views/products/product.blade.php
+++ src/afp2/resources/views/products/product.blade.php
@@ -1,2 +1,32 @@
+@extends('layout')
 
-  <p>This is product {{ $product->id }} {{ $product->name }}</p>
+@section('content')
+<div class="container">
+  <h2>{{$product->name}}</h2>
+  <p class="product-brand">{{$product->brand}} </p>
+  <div class="row">
+    <div class="col-md-8">
+      <img src="{{asset($product->img)}}" class="product-img">
+    </div>
+    <div class="col-md-4">
+      <p class="price">{{$product->price}} Ft</p>
+      <form method="post">
+        <div class="form-group">
+          <input type="number" class="form-control" id="quantity" value="1">
+        </div>
+        <button type="submit" class="btn btn-primary">Add to cart</button>
+      </form>
+    </div>
+  </div>
+
+
+</div>
+
+<h2>Description</h2>
+<hr>
+<div class="container">
+{!! $product->description !!}
+</div>
+
+
+@endsection
