32282c8fd45e52183ae51af6f9144d88ab465e16
agnespapp0729 <agnespapp99@gmail.com>
2021-03-25 12:37:21 +0100

    Formatted the table

--- src/afp2/resources/views/users/cart.blade.php
+++ src/afp2/resources/views/users/cart.blade.php
@@ -1,31 +1,31 @@
 <div class="container">
-<table class="table">
-  <thead>
-    <tr>
-      <th scope="col">#</th>
-      <th scope="col"></th>
-      <th scope="col"></th>
-      <th scope="col"></th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <th scope="row">1</th>
-      <td></td>
-      <td></td>
-      <td></td>
-    </tr>
-    <tr>
-      <th scope="row">2</th>
-      <td></td>
-      <td></td>
-      <td></td>
-    </tr>
-    <tr>
-      <th scope="row">3</th>
-      <td colspan="2"></td>
-      <td></td>
-    </tr>
-  </tbody>
-</table>
+    <table class="table">
+    <thead>
+        <tr>
+        <th scope="col">#</th>
+        <th scope="col"></th>
+        <th scope="col"></th>
+        <th scope="col"></th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+        <th scope="row">1</th>
+        <td></td>
+        <td></td>
+        <td></td>
+        </tr>
+        <tr>
+        <th scope="row">2</th>
+        <td></td>
+        <td></td>
+        <td></td>
+        </tr>
+        <tr>
+        <th scope="row">3</th>
+        <td colspan="2"></td>
+        <td></td>
+        </tr>
+    </tbody>
+    </table>
 </div>
