94a38d5236909971236b059d4287bce4adea7aa8
Tóbi Csanádi <csanadi.tobias@gmail.com>
2021-04-10 18:47:15 +0200

    Added product listing for cart view

--- src/afp2/resources/views/users/cart.blade.php
+++ src/afp2/resources/views/users/cart.blade.php
@@ -1,31 +1,39 @@
+@extends('layout')
+
+@section('content')
+
+<h2>Your cart</h2>
+
 <div class="container">
     <table class="table">
     <thead>
         <tr>
         <th scope="col">#</th>
-        <th scope="col"></th>
-        <th scope="col"></th>
+        <th scope="col">Product name</th>
+        <th scope="col">Amount</th>
+        <th scope="col">Price</th>
         <th scope="col"></th>
         </tr>
     </thead>
     <tbody>
+
+    @foreach($cart as $cartitem)
+
         <tr>
-        <th scope="row">1</th>
-        <td></td>
-        <td></td>
-        <td></td>
+            <th scope="row">{{ $loop->index}}</th>
+            <td>{{ $cartitem->product->brand }} {{ $cartitem->product->name }}</td>
+            <td>{{ $cartitem->amount }}</td>
+            <td>{{ $cartitem->amount * $cartitem->product->price }}</td>
+            <td>
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-circle" viewBox="0 0 16 16">
+                    <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+                    <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
+                </svg>
+            </td>
         </tr>
-        <tr>
-        <th scope="row">2</th>
-        <td></td>
-        <td></td>
-        <td></td>
-        </tr>
-        <tr>
-        <th scope="row">3</th>
-        <td colspan="2"></td>
-        <td></td>
-        </tr>
+    @endforeach
     </tbody>
     </table>
 </div>
+
+@endsection
