78ac505c7e120af2d910d662b0188f70e8081302
Fügedi Csaba <fugedic@gmail.com>
2021-04-12 16:56:36 +0200

    Added userList.blade.php

---
+++ src/afp2/resources/views/users/userList.blade.php
@@ -0,0 +1,15 @@
+@extends('layout')
+@section('content')
+
+<div class="register_background">
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                asd
+            </div>
+        </div>
+    </div>
+</div>
+
+
+@endsection
