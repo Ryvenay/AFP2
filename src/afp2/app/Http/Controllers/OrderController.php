bc081219dcfc536f3d3edaf7a770da18b6552b91
Tóbi Csanádi <csanadi.tobias@gmail.com>
2021-05-05 17:15:17 +0200

    Added order function to OrderController

--- src/afp2/app/Http/Controllers/OrderController.php
+++ src/afp2/app/Http/Controllers/OrderController.php
@@ -3,8 +3,47 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Cart;
+use App\Models\Product;
+use App\Models\Order;
+use App\Models\Package;
+use Illuminate\Support\Facades\Auth;
 
 class OrderController extends Controller
 {
-    //
+
+    public function show() {
+
+    }
+
+    public function make() {
+
+        $items = CartController::getCartItems();
+        $user = Auth::user();
+
+        $order = new Order;
+
+        $order -> user_id = $user->id;
+        $order -> billing_address = $user->billing_address;
+        $order -> shipping_address = $user->shipping_address;
+        $order->status = 0;
+
+        $order ->save();
+
+        foreach ($items as $item) {
+            Package::Create([
+                "product_id" => $item->product_id,
+                "order_id" => $order->id,
+                "amount" => $item->amount
+            ]);
+        }
+
+        Cart::where('user_id', $user->id)->delete();
+
+        return redirect('/');
+    }
+
+    private function getOrderById() {
+
+    }
 }
