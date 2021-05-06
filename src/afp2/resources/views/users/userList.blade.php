a13955d89c51256300591426c38c4525c241f35a
Tóbi Csanádi <csanadi.tobias@gmail.com>
2021-05-06 13:08:45 +0200

    Added user listing frontend

--- src/afp2/resources/views/users/userList.blade.php
+++ src/afp2/resources/views/users/userList.blade.php
@@ -1,14 +1,37 @@
 @extends('layout')
 @section('content')
 
-<div class="register_background">
-    <div class="container">
-        <div class="row">
-            <div class="col">
-                asd
-            </div>
-        </div>
-    </div>
+<h2>Users</h2>
+<hr>
+
+<div class="container">
+    <table class="table">
+        <thead>
+            <tr>
+                <th scope="col">user id</th>
+                <th scope="col">username</th>
+                <th scope="col">First name</th>
+                <th scope="col">Last name</th>
+                <th scope="col">email</th>
+                <th scope="col">level</th>
+            </tr>
+        </thead>
+
+
+        <tbody>
+            @foreach($users as $user)
+                <tr>
+                    <th>{{$user -> id}}</td>
+                    <td>{{$user -> username}}</td>
+                    <td>{{$user -> first_name}}</td>
+                    <td>{{$user -> last_name}}</td>
+                    <td>{{$user -> email}}</td>
+                    <td>{{$user -> level}}</td>
+                </tr>
+            @endforeach
+        </tbody>
+
+    </table>
 </div>
 
 
